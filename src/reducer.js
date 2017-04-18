0fcf366e76ae65085dbfb19660c2b979284d3761
LongLH <d@dapps.me>
2017-04-18 11:17:52 +0700

    develop core logic and reducer with TDD style

---
+++ src/reducer.js
@@ -0,0 +1,18 @@
+import {setEntries, next, vote, INITIAL_STATE} from './core';
+
+export default function reducer(state = INITIAL_STATE, action) {
+	if (!state) {
+		state = Map();
+	}
+
+	switch (action.type) {
+		case 'SET_ENTRIES':
+			return setEntries(state, action.entries);
+		case 'NEXT':
+			return next(state);
+		case 'VOTE':
+			return vote(state, action.entry);
+	}
+
+	return state;
+}
